6496a157e3df787d8c207573f1ccf6b6194d4115
Florian Oellerich <f.oellerich@epages.com>
2016-10-04 18:36:10 +0200

    Find StadtRad by location
    Find bikes near the users location in a given range.

--- app/src/main/java/de/falkentavio/speechrecognizer/Actions/StadtradAction.java
+++ app/src/main/java/de/falkentavio/speechrecognizer/Actions/StadtradAction.java
@@ -1,6 +1,7 @@
 package de.falkentavio.speechrecognizer.Actions;
 
 import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -42,23 +43,59 @@ public class StadtradAction implements Action {
         this.stations = resultObject.getJSONArray("marker");
     }
 
+    @NonNull
     @SuppressLint("DefaultLocale")
-    private Boolean hasFreeBikes(JSONObject station) throws JSONException {
+    private Integer getFreeBikes(JSONObject station) throws JSONException {
         JSONObject hal2option = station.getJSONObject("hal2option");
         JSONArray bikes = hal2option.getJSONArray("bikelist");
         if (bikes.length() > 0) {
-            Log.d(TAG, String.format("%d bikes found", bikes.length()));
-            return true;
+//            Log.d(TAG, String.format("%d bikes found", bikes.length()));
+            return bikes.length();
         }
-        Log.d(TAG, "no bikes found");
-        return false;
+//        Log.d(TAG, "no bikes found");
+        return 0;
     }
 
-    private void t() throws JSONException {
+    private float distanceToStation(JSONObject station) throws JSONException {
+        Location stationLocation = new Location("station");
+        stationLocation.setLatitude(station.getDouble("lat"));
+        stationLocation.setLongitude(station.getDouble("lng"));
+        Location myLocation = new Location("self");
+        myLocation.setLatitude(53.5542332);
+        myLocation.setLongitude(9.9786355);
+        return myLocation.distanceTo(stationLocation);
+    }
+
+    protected static String parseStationName(String source) {
+        return source.replace("&nbsp;", " ").replaceFirst("\\d{4,}\\s", "");
+    }
+
+    private String t() throws JSONException {
+        ArrayList<JSONObject> nearbyStations = new ArrayList<JSONObject>();
+        String nearestStation = "Keine Räder in der Nähe gefunden";
+        float nearestDistance = 10000;
         for(int i = 0; i < this.stations.length(); i++) {
             JSONObject station = this.stations.getJSONObject(i);
-            hasFreeBikes(station);
+            JSONObject hal2option = station.getJSONObject("hal2option");
+            String stationName = parseStationName(hal2option.getString("tooltip"));
+            Integer freeBikes = getFreeBikes(station);
+            float distance = distanceToStation(station);
+//            Log.d(TAG, "Distance: " + distance);
+            if (distance < 800) {
+                if (freeBikes > 0) {
+                    Log.d(TAG, stationName + " hat " + freeBikes + " freie Bikes");
+                    nearbyStations.add(station);
+                    if (distance < nearestDistance) {
+                        nearestStation = stationName + " hat " + freeBikes + " freie Bikes";
+                        nearestDistance = distance;
+                    }
+                } else {
+                    Log.d(TAG, stationName + " hat keine freien Bikes");
+                }
+            }
         }
+        Log.d(TAG, "Nearby stations: " + nearbyStations.size());
+        return nearestStation;
     }
 
     public StadtradAction() {
@@ -87,7 +124,7 @@ public class StadtradAction implements Action {
         try {
             try {
                 this.fetchStations();
-                this.t();
+                return this.t();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
